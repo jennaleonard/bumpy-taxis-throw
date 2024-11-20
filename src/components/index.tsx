c936813f02cc0714e81e410ae0bedf328b34f546
jennaleonard <jleonardweb@gmail.com>
2024-11-19 19:15:16 -0800

    completed homepage

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,10 +5,14 @@ import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
 
 export {
   UpcomingEvents,
   DealsChart,
+  DashboardTotalCountCard,
+  LatestActivities,
   UpcomingEventsSkeleton,
   AccordionHeaderSkeleton,
   KanbanColumnSkeleton,
